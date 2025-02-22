b0449bd06864f0c5928d8a2176004ad206d84560
colaola20 <soryo@farmingdale.edu>
2025-02-21 22:02:40 -0500

    Finding a minimum number in an array using fork()

---
+++ min_number.cpp
@@ -0,0 +1,85 @@
+#include <iostream> 
+#include <string.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h> 
+#include <cstdlib>
+#include <time.h>
+//using namespace std; 
+  
+int main() { 
+    //Using current system time as seed value
+    int i;
+    int arr [20];
+    unsigned int seed = time(0);
+    int start, end;
+    int min_num;
+
+    printf("The array: ");
+    for (i = 0; i < 20; i++) {
+        arr[i] = rand_r(&seed) % 101;
+        printf("%d ", arr[i]);
+        }
+    printf("\n");
+
+
+    int fd[2];
+
+    if  (pipe(fd)==-1) {
+        return 1;
+    }
+
+    //Creating a child process
+    int id = fork();
+
+    if (id == -1) {
+        return 2;
+    }
+
+    if (id != 0) {
+        start=0;
+        min_num = arr[start];
+        end= 10;
+        printf("parent loop from 0 to  : %d\n",end);
+    }
+    else {
+        start= 10;
+        min_num = arr[start];
+        end = 20;
+        printf("child loop from  : %d\n",start);
+    }
+
+    
+    //Finding a minimun in a half of the array
+    for (i = start;i < end;i++){
+        if (arr[i] < min_num) {
+            min_num = arr[i];
+        }
+    }
+
+    printf("The min num is: %d\n", min_num);
+
+    if (id==0) {
+        printf("child's process id is  %d\n",getpid());
+        write(fd[1],&min_num,sizeof(min_num));
+        close(fd[1]);
+    }
+    else{
+        printf("parent's process id is  %d\n",getpid());
+        close(fd[1]);
+        int childMin;
+        read(fd[0], &childMin,sizeof(int));
+        close(fd[0]);
+        int minimum;
+        if (childMin < min_num) {
+            minimum = childMin;
+        } else {
+            minimum = min_num;
+        }
+
+        printf("The minimum number is : %d\n",minimum);
+        }
+
+    return 0;
+}
